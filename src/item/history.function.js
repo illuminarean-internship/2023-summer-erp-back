fa6efb4a20ff57a60abd83687b8d422addc50204
briley0828 <boyeon.kim@illuminarean.com>
2023-08-03 16:58:45 +0900

    fix error with parsing

--- src/item/history.function.js
+++ src/item/history.function.js
@@ -1,12 +1,12 @@
 
-//  "2023. 8. 2.-2023. 8. 2./Office/Remark1", -> object  
+//  "2023. 8. 2.-2023. 8. 2./Office/Remark1", -> object
 export function parseToObjectList(stringList) {
   const objectList = [];
   for (let index = 0; index < stringList.length; index +=1 ) {
       const splitedStr = stringList[index].split('/');
       const rowId = index;
       const startDate = splitedStr[0] ? convertDateString(splitedStr[0]) : null;
-      const endDate = splitedStr[1] ? convertDateString(splitedStr[1]) : null;
+      const endDate = splitedStr[1].length > 1 ? convertDateString(splitedStr[1]) : null;
       const historyLocation = splitedStr[2] ? splitedStr[2] : null;
       const historyRemark = splitedStr[3] ? splitedStr[3] : '';
   
@@ -25,11 +25,13 @@ export function parseToObjectList(stringList) {
   
  export function convertDateString(dateString) {
     const parts = dateString.split('.').map(part => part.trim());
+    if(parts[1]){
     const year = parts[0];
     const month = parts[1].padStart(2, '0');
     const day = parts[2].padStart(2, '0');
   
-    return `${year}-${month}-${day}`;
+    return `${year}-${month}-${day}`;}
+    else return '';
   }
   
   

b687fa6fc24b7eb536241e8a541696a21ebf8bb4
prestonfutaba <gambitnf6@gmail.com>
2023-08-03 00:00:56 -0700

    minor changes

--- index.route.js
+++ index.route.js
@@ -1,39 +1,25 @@
-/*import express from 'express';
+import express from 'express';
 import authRoute from './src/auth/auth.route.js';
 import userRoute from './src/user/user.route.js';
+import teamRoute from './src/user/team.route.js';
+import projRoute from './src/user/project.route.js';
 import itemRoute from './src/item/sample/item.route.js';
-
+import bookRoute from './src/item/book/book.route.js';
+import swRoute from './src/item/software/sw.route.js';
+import laptopRoute from './src/item/laptop/laptopRoute.js';
 const router = express.Router();
 
 router.get('/health-check', (req, res) => res.send('OK'));
 
 router.use('/auth', authRoute);
 router.use('/users', userRoute);
+router.use('/teams', teamRoute);
 router.use('/items', itemRoute);
+router.use('/projs', projRoute);
+
+
+router.use('/books', bookRoute);
+router.use('/software', swRoute);
+router.use('/laptops', laptopRoute);
 
 export default router;
-*/
-
-const express = require('express');
-const bodyParser = require('body-parser');
-const mongoose = require('mongoose');
-const bookRoute = require('./routes/bookRoute');
-
-const app = express();
-const port = 3000;
-
-app.use(bodyParser.json());
-
-// Connect to your MongoDB database (replace 'your_db_url' with your actual MongoDB URL)
-mongoose.connect('mongodb://your_db_url/book_management', {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
-
-// Use the bookRoute for /api/books endpoint
-app.use('/api/books', bookRoute);
-
-app.listen(port, () => {
-  console.log(`Server is running on http://localhost:${port}`);
-});
-
